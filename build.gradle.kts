d7d0925dfa009679b9223b1dfc10d967d7a8c7e2
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2024-10-16 13:21:37 -0300

    Update build.gradle.kts
    adicionei as extensões do token

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,13 +29,18 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	val jjwt = "0.12.5"
+	implementation("io.jsonwebtoken:jjwt-api:${jjwt}")
+	implementation("io.jsonwebtoken:jjwt-jackson:${jjwt}")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:${jjwt}")
 
 	runtimeOnly("com.h2database:h2") // H2 como banco de dados em memória
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("io.mockk:mockk:1.13.5") // MockK para testes
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	implementation("io.mockk:mockk:1.13.5") // MockK para testes
+	runtimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
